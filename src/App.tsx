84c40683905b91d6e83e0cc9497d8ec63a73c4dd
Natanael <natanaeldepaulo123@gmail.com>
2022-06-05 23:41:30 -0300

    commite initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Aside } from './components/Aside';
 
 function App() {
   return (
     <div className="App">
-      <h1>Initial</h1>
+     <Aside/>
     </div>
   );
 }
---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,28 @@
+import * as icon from 'react-icons/md'
+
+export function Aside() {
+  return(
+    <>
+      <ul>
+        <li>
+
+        </li>
+        <li>
+          <icon.MdHome size={20}/>
+        </li>
+        <li>
+          <icon.MdLocalDining size={20}/>
+        </li>
+        <li>
+          <icon.MdLocalGroceryStore size={20}/>
+        </li>
+        <li>
+          <icon.MdLocationPin size={20}/>
+        </li>
+        <li>
+
+        </li>
+      </ul>
+    </>
+  )
+}
