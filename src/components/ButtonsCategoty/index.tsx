856b633b0c8cc233cd09fddff93d1d683ff34da8
Natanael <natanaeldepaulo123@gmail.com>
2022-10-11 11:43:40 -0300

    refactor: Ordering of buttons

--- src/components/ButtonsCategoty/index.tsx
+++ src/components/ButtonsCategoty/index.tsx
@@ -1,30 +1,50 @@
+import { MdClose } from 'react-icons/md';
 import { PropsFoods } from '../../services/server/food/types';
-import { ButtonsContainer } from './styles';
+import { Container, ItemsButtons } from './styles';
 
 interface IProps {
   foods: PropsFoods[];
+  showMenu: () => void
+  active: boolean
 }
 
-export function ButtonsCategory ({ foods } : IProps){
+export function ButtonsCategory ({ foods, showMenu, active } : IProps){
+
+  const carne = foods.map(product => product).filter(product => product.category === "Carne")
+
+  const categoryDisplayButtons = foods.map(product => product ).reduce((initialValue : String[], currentValue) => {
+    return initialValue.includes(currentValue.category)? initialValue : [...initialValue, currentValue.category]
+  },[]).sort((a,b) => {
+      if(a > b){
+        return 1
+      } else if(a < b) {
+        return -1
+      } else {
+        return 0      
+      }
+  })
 
 
-  const categoryDisplayButtons = foods.map((product) => product).reduce((initialValue : String[], currentValue) => {
-    return initialValue.includes(currentValue.category)? initialValue : [...initialValue, currentValue.category ]
-  },['Todos'])
+
+
+
+  categoryDisplayButtons.unshift("Todos")
 
   return(
     <>
-      <ButtonsContainer>
-        <ul>
+      <Container active={active}>
+        <MdClose className='iconClose' width={24} height={24} onClick={showMenu} style={{cursor: 'pointer'}}/>
+        <ItemsButtons>
           {categoryDisplayButtons.map((item, index) => (
             <li key={index}>
-              <button  className='categoryButtons'>
+              <button  className='categoryButtons' onClick={() => carne}>
                 {item}
               </button>
             </li>
           ))}
-        </ul>
-      </ButtonsContainer>
+        </ItemsButtons>
+      </Container>
+     
     </>
   )
 }
--- src/components/ButtonsCategoty/styles.ts
+++ src/components/ButtonsCategoty/styles.ts
@@ -1,25 +1,59 @@
 import styled from "styled-components";
 
-export const ButtonsContainer = styled.div`
-  display: flex;
-  margin-top: 1em;
+interface Props{
+  active: boolean;
+}
+
+
+export const Container = styled.aside<Props>`
+  position: relative;
+  max-width: 25%;
+  display: ${ ({active}) => active? 'flex' : 'none' };
+  align-items: center;
   justify-content: center;
+  flex-direction: column;
   
   
+  .iconClose{
+    height: 2em;
+    width: 3em;
+    position: absolute;
+    right: 0;
+  }
 
-  ul{
+  @media (min-width: 678px) {
+    max-width: 100%;
     display: flex;
-    gap: 0.5em 1em;    
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
+    position: initial;  
 
-    button{
-      width: 8em;
-      padding: .5em 1em;
-      background: black;
-      color: white;
-      
+    .iconClose{
+      display: none;
     }
+  }
+`
+
+
+export const ItemsButtons = styled.ul`
+  display: flex;
+  gap: 0.3em;    
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, 32%);
+
+  @media (min-width: 678px) {
+    position: initial;
+    transform: none;
+
+  }
+
+  button{
+    width: 6em;
+    padding: 0.5em 0;
+    background: black;
+    color: white;
+    border-radius: 2em;
   }
 `
