26d055959b0a0778013f83801d9278763f791c16
Natanael <natanaeldepaulo123@gmail.com>
2022-11-07 00:20:20 -0300

    feat: Created function for clean string

--- src/util/format.ts
+++ src/util/format.ts
@@ -2,3 +2,9 @@ export const { format: formatPrice } = new Intl.NumberFormat('pt-br', {
   style: 'currency',
   currency: 'BRL',
 });
+
+
+
+export const cleanString =  (value: string) => {
+  return value?.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+}
